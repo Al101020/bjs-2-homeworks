a128aac837edf3373e74685471805f1f5b253496
alexgithub <alexgithub@yandex.ru>
2023-01-28 15:51:17 +1000

    модификация 1.base-concepts/task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,42 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+
+  let d = Math.pow(b,2) - 4 * a * c;
+  if (d > 0) {
+    let x1 = (-b + Math.sqrt(d) )/(2*a);
+    let x2 = (-b - Math.sqrt(d) )/(2*a);
+    arr.push(x1);
+    arr.push(x2);
+  } else if (d < 0) {
+
+  } else if (d === 0) {
+    let x = - (b / 2 * a);
+
+    arr.push(x);
+  }
   
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (isNaN(percent)) {
+    return false;
+  } else if (isNaN(contribution)) {
+    return false;
+  } else if (isNaN(amount)) {
+    return false;
+  } else if (isNaN(countMonths)) {
+    return false;
+  }
+
+  let P = percent/100/12
+  console.log('P = ' + P);
+  let S = amount - contribution;
+  let n = countMonths;
+  let monthlyPayment = S * (P + (P / (((1 + P)**n) - 1)));
+  let totalAmount = monthlyPayment * n
+
+  return +totalAmount.toFixed(2);
+
 }
