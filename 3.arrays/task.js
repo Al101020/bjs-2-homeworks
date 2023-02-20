9f21e889de581e75a387162ba3b0b752b99278f1
alexgithub <alexgithub@yandex.ru>
2023-02-20 16:48:50 +1000

    modified:   3.arrays/task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,37 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.every((element, index) => arr1.length === arr2.length && element === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+    if (users.length === 0) {
+      return 0;
+    } else if(gender !== "мужской" && gender !== "женский") {
+      return 0;    
+    } else if (gender === "мужской") {
+      let result = people.filter((Male) => Male.gender === "мужской").map((ages) => ages.age).reduce((acc, item, index, arr) => {
+        acc += item;
+        if (index === arr.length -1) {
+        return acc / arr.length;
+        }
+        return acc;
+        }, 0)
+      return result;
   
-}
+    } else if (gender === "женский") {
+      let result = people.filter((female) => female.gender === "женский").map((maleAge) => maleAge.age).reduce((acc, item, index, arr) => {
+        acc += item;
+        if (index === arr.length -1) {
+        return acc / arr.length;
+        }
+        return acc;
+        }, 0)
+      return result;
+  
+    }
+  };
+
+
+
+//function getUsersNamesInAgeRange(users, gender) {
+//  
+//}
