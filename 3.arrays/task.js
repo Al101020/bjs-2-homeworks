771330253ad15a10631dec96a8201e9e16da3322
alexgithub <alexgithub@yandex.ru>
2023-03-03 12:30:33 +1000

    modified 2023.03.03: 3.arrays/task.js & 4.objects/task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -4,32 +4,24 @@ function compareArrays(arr1, arr2) {
 
 function getUsersNamesInAgeRange(users, gender) {
   if (users.length === 0 || gender !== "мужской" && gender !== "женский") {
-    return 0;   
-  } else if (gender === "мужской") {
-    let result = users.filter((user) => user.gender === "мужской").map((ages) => ages.age).reduce((acc, item, index, arr) => {
-      acc += item;
-      if (index === arr.length -1) {
-      return acc / arr.length;
-      }
-      return acc;
-      }, 0)
+      return 0;   
+    } else if (gender === "женский") {
+       result = users.filter((user) => user.gender === "женский").reduce((acc, cur) => acc + cur.age, 0) / users.filter((user) => user.gender === "женский").map((ages) => ages.age).length;
     return result;
-
-  } else if (gender === "женский") {
-    let result = users.filter((user) => user.gender === "женский").map((ages) => ages.age).reduce((acc, item, index, arr) => {
-      acc += item;
-      if (index === arr.length -1) {
-      return acc / arr.length;
-      }
-      return acc;
-      }, 0)
+    }else if (gender === "мужской") {
+       result = users.filter((user) => user.gender === "мужской").reduce((acc, cur) => acc + cur.age, 0) / users.filter((user) => user.gender === "мужской").map((ages) => ages.age).length;
     return result;
-
-  }
-};
-
-
+    }
+}
 
 //function getUsersNamesInAgeRange(users, gender) {
-//  
+//  if (users.length === 0 || gender !== "мужской" && gender !== "женский") {
+//      return 0;   
+//    } else if (gender === "женский") {
+//       result = users.filter((user) => user.gender === "женский").reduce((acc, cur) => acc + cur.age, 0) / users.filter((user) => user.gender === "женский").reduce((acc, cur) => acc + 1, 0);
+//    return result;
+//    }else if (gender === "мужской") {
+//       result = users.filter((user) => user.gender === "мужской").reduce((acc, cur) => acc + cur.age, 0) / users.filter((user) => user.gender === "мужской").reduce((acc, cur) => acc + 1, 0);
+//    return result;
+//    }
 //}
