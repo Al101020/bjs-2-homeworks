ebf20789a9536117e9191284be4a550c6b669846
alexgithub <alexgithub@yandex.ru>
2023-04-10 09:53:05 +1000

    again status 6.exception-closure/task.js

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,14 +1,37 @@
-﻿function parseCount(meaning) {
+function parseCount(meaning) {
     let parsen = Number.parseFloat(meaning);
-    if (Number.isNaN(parsen)) throw new Error("Невалидной значение");
-    if (parsen === parsen) return parsen;
+    if (Number.isNaN(parsen)) {
+        throw new Error("Невалидное значение");
+    }
+    return parsen;
 }
-
-// - parseCount("ыфва")
 
 function validateCount(meaning) {
-    return parseCount(meaning);
+    try {
+        let parsen = parseCount(meaning);
+        return parsen;
+    } catch {
+        throw new Error("И тут невалидное значение");
+    }
 }
+
+
+
+// function validateCount(meaning) {
+//     try {
+//         let pars = Number.parseFloat(meaning);
+//         let err;
+// //        console.log(pars);
+//         if (Number.isNaN(pars)) {
+//             let err = new Error;
+//             throw err; 
+// //            console.log(err);
+//         }
+//         return pars;
+//     } catch {
+//         throw err("Невалидное значение");        
+//     }
+// }
 
 // - validateCount("ыфва")
 
@@ -28,11 +51,41 @@ class Triangle {
     }
 }
 
-function getTriangle(x, y, e) {
+function getTriangle(a, b, c) {
     try {
-        return new Triangle(x, y, e);
+        class Triangle {
+            constructor(a, b, c) {
+                if (a + b < c || b + c < a || c + a < b) throw new Error("Треугольник с такими сторонами не существует");
+                this.p = a + b + c;
+                this.s = +(Math.sqrt(this.p / 2 * (this.p / 2 - a) * (this.p / 2 - b) * (this.p / 2 - c)) ).toFixed(3);
+            }
+        
+            get perimeter() {
+                return this.p;
+            }
+        
+            get area() {
+                return this.s;
+            }
+        }
+        return new Triangle(a, b, c);
     } catch {
-        return new Triangle(x, y, e),   this.s = "Ошибка! Треугольник не существует",   this.p = "Ошибка! Треугольник не существует";
+        class Triangle {
+            constructor(a, b, c) {
+                this.p = "Ошибка! Треугольник не существует";
+                this.s = "Ошибка! Треугольник не существует";
+            }
+        
+            get perimeter() {
+                return this.p;
+            }
+        
+            get area() {
+                return this.s;
+            }
+        }
+        return new Triangle(a, b, c); 
+        // return new Triangle(a, b, c);//,   this.s = "Ошибка! Треугольник не существует",   this.p = "Ошибка! Треугольник не существует";
     }
 }
 
@@ -43,5 +96,6 @@ function getTriangle(x, y, e) {
 
 // const triangle1 = new Triangle(2,5,5);
 // const triangle2 = new Triangle(6,10,15);
+//let triangle3 = [getTriangle(10, 11, 12)];
 
-let triangle3 = [getTriangle(10, 11, 12)];
+let qwq = getTriangle(2,5,5);
